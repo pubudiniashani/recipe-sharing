a4abce89111ff1cd58aae1d64e4e00aff8fd02ca
pubudiniashani <pubudiniashani2000@gmail.com>
2025-04-09 22:56:57 +0530

    login updated

---
+++ src/pages/Login.tsx
@@ -0,0 +1,22 @@
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export const Login(){
+    return(
+        <>
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Login</Button>
+                </CardActions>
+            </Card>
+
+        </>
+    )
+}
