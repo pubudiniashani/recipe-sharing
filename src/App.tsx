c7d376446aa2709cc12b4f9db8a99311e46b8c2c
pubudiniashani <pubudiniashani2000@gmail.com>
2025-04-10 13:02:24 +0530

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes} from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
 import Signup from "./pages/Signup.tsx";
 import Login from './pages/Login.tsx';
+import {RootLayout} from "./components/RootLayout.tsx";
 
 
 function App() {
 
+
+    const routes = createBrowserRouter([
+
+        {
+            path: "/",
+            element: <Login />,
+        },
+        {
+            path: "/signup",
+            element: <Signup />,
+        },
+
+        {
+            path : '',
+            element : <RootLayout/>,
+            children : [
+                { path : '/home', element : <Home/>},
+
+            ]
+        } ,
+    ])
+
+
   return (
     <>
 
-        <BrowserRouter>
-            <Routes>
-                <Route path="/signup" element={<Signup/>} />
-                <Route path="/login" element={<Login/>} />
-                <Route path="/home" element={<Home/>} />
+        {/*<BrowserRouter>*/}
+        {/*    <Routes>*/}
+        {/*        <Route path="/signup" element={<Signup/>} />*/}
+        {/*        <Route path="/login" element={<Login/>} />*/}
+        {/*        <Route path="/home" element={<Home/>} />*/}
 
-            </Routes>
+        {/*    </Routes>*/}
 
-        </BrowserRouter>
-    </>
+        {/*</BrowserRouter>*/}
+
+        <RouterProvider router={routes}/>
+
+
+        </>
   )
 }
 
